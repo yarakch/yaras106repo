ebbe9f84c72e338e1d965a881aa21dcdcc61920f
yarakch <ycurrier@uci.edu>
2023-02-22 16:28:23 -0800

    Removed comments and revised doc strings.

--- UpdatedSignalDetection.py
+++ UpdatedSignalDetection.py
@@ -3,40 +3,42 @@ from scipy.special import ndtri
 
 class SignalDetection:
   def __init__(self,hit,miss,fa,cr):
-    # Define variables
+    """Initializes the class object using the self, hit, miss, false alarm, and correct rejection variables."""
     self.__hit = hit
     self.__miss = miss
     self.__fa = fa
     self.__cr = cr
 
   def __str__(self):
-    "Returns the class as a labeled list to enable printing and error detection."
+    """Returns the class as a labeled list to enable printing and error detection."""
     return f"hits: {self.__hit}, misses: {self.__miss}, false alarms: {self.__fa}, correct rejections: {self.__cr}" 
 
-    #self.hr = (hit / (hit + miss))
-    #self.far = (fa / (fa + cr))
-  def hitRate(self): # should this be private? ask Weds
+  def hitRate(self): 
+    """Calculates the hit rate given the hits and misses."""
     self.__hr = (self.__hit / (self.__hit + self.__miss))
     return self.__hr
 
-  def falseAlarmRate(self): # should this be private? ask Weds
+  def falseAlarmRate(self):
+    """Calculates the false alarm rate given the hits and misses."""
     self.__far = (self.__fa / (self.__fa + self.__cr))
     return self.__far
 
   def d_prime(self):
-    # Calculates the d-Prime value
+    """Calculates the d-Prime value given the hit rate and false alarm rate."""
     self.__dprime = (ndtri(self.hitRate()) - ndtri(self.falseAlarmRate()))
-    return self.__dprime #ask about ndtri vs norm.ppf
+    return self.__dprime
   
   def criterion(self):
-    # Calculates the Criterion value
+    """Calculates the Criterion value given the hit rate and false alarm rate."""
     self.__criterion = (-0.5 * (ndtri(self.hitRate()) + ndtri(self.falseAlarmRate())))
     return self.__criterion
 
   def __add__(self, other):
+    """Overloads the addition operator to add two class objects to one another."""
     return SignalDetection(self.__hit + other.__hit, self.__miss + other.__miss, self.__fa + other.__fa, self.__cr + other.__cr)
 
   def __mul__(self, scalar):
+    """Overloads the multiplication operator to multiply a class object by a scalar."""
     return SignalDetection(self.__hit * scalar, self.__miss * scalar, self.__fa * scalar, self.__cr * scalar) 
 
 def plot_roc(*objects): # * specifies unknown number of objects
