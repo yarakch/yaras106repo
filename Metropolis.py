cb40b51ab3396e9f0335269b2613407fd79978f8
yarakch <ycurrier@uci.edu>
2023-03-15 14:03:42 -0700

    Scaffolded class with functions, docstring, and constructor method.

---
+++ Metropolis.py
@@ -0,0 +1,25 @@
+class Metropolis:
+  def __init__(self, logTarget, initialState):
+    """Initializes the class object using the self, a target function, and an initial state."""
+    self.logTarget = logTarget
+    self.initialState = initialState
+  
+  def __accept(self, proposal):
+    """Checkes whether to accept or reject proposed value based on acceptance probability."""
+
+    return
+
+  def adapt(self):
+    """Performs the adaptation phase of the Metropolis algorithm."""
+
+    return self
+  
+  def sample(self, n):
+    """Generates n samples from the target distribution using the Metropolis algorithm."""
+
+    return self
+
+  def summmary(self):
+    """Returns a dictionary or structure containing the mean and a 95% credible interval of generated samples."""
+
+    return summ
