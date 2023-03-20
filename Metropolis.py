20a9b422e1d16dda3fb6c850bfd27bc377e9668a
yarakch <ycurrier@uci.edu>
2023-03-19 18:56:36 -0700

    Deleted acceptanceProb and revised init.

--- Metropolis.py
+++ Metropolis.py
@@ -2,12 +2,8 @@ class Metropolis:
   def __init__(self, logTarget, initialState):
     """Initializes the class object using the self, a target function, and an initial state."""
     self.logTarget = logTarget
-    self.initialState = initialState
-
-def acceptanceProb(initialState, proposedState):
-    """Calculates acceptance probability given initial and proposed states."""
-    self.acceptProb = min(1, (exp((proposedState/initialState))))
-    return self.acceptProb
+    self.currentState = initialState
+    self.samples = []
   
   def __accept(self, proposal):
     """Checkes whether to accept or reject proposed value based on acceptance probability."""
