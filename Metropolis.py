1622f636ede26b1ea8774fc48cfcfc3f5f691acc
yarakch <ycurrier@uci.edu>
2023-03-19 19:42:13 -0700

    Debugging.

--- Metropolis.py
+++ Metropolis.py
@@ -4,19 +4,19 @@ class Metropolis:
     self.logTarget = logTarget
     self.currentState = initialState
     self.samples = []
-    self.accepted = 0
-    self.proposed = 0
+    self.__accepted = 0
+    self.__proposed = 0
   
   def __accept(self, proposal):
     """Checkes whether to accept or reject proposed value based on acceptance probability."""
     acceptanceProb = min(0, self.logTarget(proposal) - self.logTarget(self.currentState))
     if acceptanceProb > numpy.log(np.random.uniform()): 
       self.currentState = proposal
-      self.accepted += 1 
-      self.proposed += 1
+      self.__accepted += 1 
+      self.__proposed += 1
       return True
     else: 
-      self.proposed += 1
+      self.__proposed += 1
       return False
 
   def adapt(self):
@@ -34,7 +34,8 @@ class Metropolis:
       acceptanceRate = self.__accepted / self.__proposed
       proposedSD = proposedSD * ((acceptanceRate / targetRate) **1.1)
       N += 1
-    self.SD = proposedSD  
+    self.SD = proposedSD 
+    return self 
 
   def sample(self, n):
     """Generates n samples from the target distribution using the Metropolis algorithm."""
